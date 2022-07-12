cc7b2a4337dc8b306067180069aadb49fe1503c1
01ihsan <karatasihsan01@gmail.com>
2022-07-12 15:20:53 +0200

    update

--- src/test/java/jdbctests/jdbc_examples.java
+++ src/test/java/jdbctests/jdbc_examples.java
@@ -50,12 +50,27 @@ public class jdbc_examples {
         Connection connection= DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY); //move freely and read only
         ResultSet resultSet = statement.executeQuery("select * from departments");
+
         DatabaseMetaData dbMetadata = connection.getMetaData();
+
         System.out.println(dbMetadata.getUserName());
         System.out.println(dbMetadata.getDatabaseProductName());
         System.out.println(dbMetadata.getDatabaseProductVersion());
         System.out.println(dbMetadata.getDriverName());
         System.out.println(dbMetadata.getDriverVersion());
+
+        ResultSetMetaData resultSetMetaData= resultSet.getMetaData();
+
+        //how many columns do we have?
+        int colCount=resultSetMetaData.getColumnCount();
+        System.out.println(colCount);
+
+        //what is the columns' name?
+        System.out.println(resultSetMetaData.getColumnName(1));
+        System.out.println(resultSetMetaData.getColumnName(2));
+        System.out.println("-----------------------------");
+        for (int i =1; i<=resultSetMetaData.getColumnCount();i++)
+            System.out.println(resultSetMetaData.getColumnName(i));
         //close connection...
         resultSet.close();
         statement.close();
